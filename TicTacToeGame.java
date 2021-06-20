8ca3b5b299bec4b4110336cd7c58866c6827eea5
bhavani125 <girinenibhavani12@gmil.com>
2021-06-20 14:36:31 +0530

    Added: code of creating board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,18 @@
 package com.game;
 
 public class TicTacToeGame {
+
+    //constant variable
+     static char[] board = new char[10];
+    //main method
     public static void main(String[] args) {
-        System.out.println("Welcome to Game");
+        //calling method here
+        creatingBoard();
+    }
+    //creating a method for main
+    static void creatingBoard() {
+        for(int i=0;i<10; i++) {
+            board[i]='-';
+        }
     }
 }
-
-    
