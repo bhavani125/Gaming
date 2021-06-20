bce306e474bf96afff753c8920cae547cb15b82d
bhavani125 <girinenibhavani12@gmil.com>
2021-06-20 23:41:57 +0530

    Added: code for  the user move in board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class TicTacToeGame {
     //constant variable
     static char[] board = new char[10];
-    static char player,computer;
+    static char player, computer;
     public static Scanner sc = new Scanner(System.in);
+
     //main method
     public static void main(String[] args) {
         //calling method here
@@ -15,46 +16,63 @@ public class TicTacToeGame {
         playerChoice();
         //calling current board method
         currentBoard();
+        //calling player move method
+        userMove();
     }
+
     //creating a method for main
     static void creatingBoard() {
         for (int i = 0; i < 10; i++) {
             board[i] = ' ';
         }
     }
+
     //creating method for choosing option
     static void playerChoice() {
         System.out.println("choose an option x or o");
-         player = sc.next().charAt(0);
+        player = sc.next().charAt(0);
         if (player == 'x') {
-            computer='o';
+            computer = 'o';
         } else {
-            computer ='x';
+            computer = 'x';
         }
     }
+
     //creating a method to show empty board
     static void showBoard() {
 
         System.out.println("     |     |     ");
-        System.out.println("  " + board[1] + "  | " + board[2]  + "   | " + board[3] + "  ");
+        System.out.println("  " + board[1] + "  | " + board[2] + "   | " + board[3] + "  ");
         System.out.println(".....|.....|.....");
-        System.out.println("     |     |     ");
-        System.out.println("  " + board[4] + "  | " + board[5]  + "   | " + board[6] + "  ");
-        System.out.println(".................");
-        System.out.println("     |     |     ");
-        System.out.println("  " + board[7] + "  | " + board[8]  + "   | " + board[9] + "  ");
+        System.out.println("  " + board[4] + "  | " + board[5] + "   | " + board[6] + "  ");
+        System.out.println(".....|.....|.....");
+        System.out.println("  " + board[7] + "  | " + board[8] + "   | " + board[9] + "  ");
+        System.out.println("     |     |      ");
 
     }
+
     //writing a method to show current board
     private static void currentBoard() {
         int RADIX = 10;
         System.out.println("\n");
-        for( int i=1; i<10; i++) {
-            if (board[i] !='x'&&board[i] !='o')
+        for (int i = 1; i < 10; i++) {
+            if (board[i] != 'x' && board[i] != 'o')
                 board[i] = Character.forDigit(i, RADIX);
         }
         //calling show board method  here
         showBoard();
     }
-}
 
+    // creating method for user to move
+    static void userMove() {
+        System.out.println("User to select the index from 1to9");
+        int userChoice = sc.nextInt();
+        if (board[userChoice] != 'x' && board[userChoice] != 'o') {
+            board[userChoice] = player;
+        } else {
+            System.out.println("there is no free space");
+        }
+        //calling current board method
+        currentBoard();
+    }
+}
