c0e1220a175e3d2a3dca9eb7498bc8f8557c50a2
bhavani125 <girinenibhavani12@gmil.com>
2021-06-20 18:40:59 +0530

    Added: code of choosing the option for player

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,18 +1,33 @@
 package com.game;
 
-public class TicTacToeGame {
+import java.util.Scanner;
 
+public class TicTacToeGame {
     //constant variable
-     static char[] board = new char[10];
+    static char[] board = new char[10];
+    static char player,computer;
+    public static Scanner sc = new Scanner(System.in);
     //main method
     public static void main(String[] args) {
         //calling method here
         creatingBoard();
+        playerChoice();
     }
     //creating a method for main
     static void creatingBoard() {
-        for(int i=0;i<10; i++) {
-            board[i]='-';
+        for (int i = 0; i < 10; i++) {
+            board[i] = '-';
         }
     }
+    static void playerChoice() {
+        System.out.println("choose an option x or o");
+         player = sc.next().charAt(0);
+        if (player == 'x') {
+            computer='o';
+        } else {
+            computer ='x';
+        }
+    }
+
 }
+
